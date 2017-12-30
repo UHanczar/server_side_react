8ba839431bda09c24a608b7e312d6c7ded69ffd3
UHanczar <u.hanczar@gmail.com>
2017-12-30 12:59:55 +0300

    initial commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <div>I'm the Home Super component!</div>
+    <button onClick={() => console.log('Hi there')}>Hi there</button>
+  </div>
+);
+
+export default Home;
